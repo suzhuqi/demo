b30541092e3d808cfae91a576ef5a3221bd34607
suzhuqi <suzhuqi@163.com>
2019-08-04 18:38:59 +0800

    test2分支添加testBranch555方法

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -59,7 +59,7 @@ public class HelloController {
 
     @RequestMapping("/tsetBranch88")
     public String tsetBranch88(){
-         return "你好!tsetBranch88--分支";
+        return "你好!tsetBranch88--分支";
     }
 
 }
